ceb9c9741152ea3f618314b24a4020d1b6f0d18d
hansol yoon <yoondev3434@gmail.com>
2026-04-18 01:04:27 +0900

    chore: update granite setting file

--- granite.config.ts
+++ granite.config.ts
@@ -1,30 +1,20 @@
-import { defineConfig } from '@apps-in-toss/web-framework/config'
+import { defineConfig } from '@apps-in-toss/web-framework/config';
 
 export default defineConfig({
-  appName: 'momentbook-toss',
+  appName: 'my-granite-app', // 앱인토스 콘솔에서 등록한 앱 이름으로 바꿔주세요.
   brand: {
-    displayName: 'Momentbook',
-    primaryColor: '#0064FF',
-    icon: '/momentbook-icon.svg',
-  },
-  permissions: [{ name: 'photos', access: 'read' }],
-  navigationBar: {
-    withBackButton: true,
-    withHomeButton: true,
+    displayName: 'MomentBook', // 앱인토스 콘솔에 등록한 앱의 한글 이름으로 바꿔주세요.
+    primaryColor: '#81C784', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
+    icon: '', // 화면에 노출될 앱의 아이콘 이미지 주소로 바꿔주세요.
   },
   web: {
+    host: 'localhost',
     port: 5173,
     commands: {
-      dev: 'npm run dev:host',
-      build: 'npm run build:web',
+      dev: 'vite dev',
+      build: 'vite build',
     },
   },
-  webViewProps: {
-    type: 'partner',
-    bounces: false,
-    pullToRefreshEnabled: false,
-    overScrollMode: 'never',
-    allowsBackForwardNavigationGestures: false,
-  },
+  permissions: [],
   outdir: 'dist',
-})
+});
